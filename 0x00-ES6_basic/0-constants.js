9ad23da152f0973d9f1e86e1bb8e97f2feb3a48a
lelenguyasameria <somearia711@gmail.com>
2024-03-05 12:20:54 +0300

    execute 1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Updated var to const
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let'; // Updated var to let
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
