2f7fcba7bec8c395e1aae9bd1b186d213d3e2c66
lelenguyasameria <somearia711@gmail.com>
2024-04-30 21:31:21 +0300

    display message changed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create an interface for readline
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Prompt the user
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-    console.log(`Your name is: ${name}`);
-
-    // Prepare to close the application
-    console.log('This important software is now closing\n');
-
-    // Close the readline interface
-    rl.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
